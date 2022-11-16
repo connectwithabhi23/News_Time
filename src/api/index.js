b66be8834a44fef48e0e4af07ad45342173fd6c2
connectwithabhi23 <107621978+connectwithabhi23@users.noreply.github.com>
2022-11-16 22:03:07 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -8,9 +8,9 @@ export const fetchSources = async () => {
     try {
        
         // const { data: { sources } } = await axios.get(`${url}/${requestSource}${api}`);
-        const {data:{sources}} = await axios.get(`https://corsanywhere.herokuapp.com/https://newsapi.org/v2/sources?apiKey=${api}`)
+        const {data:{sources}} = await axios.get(`https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/sources?apiKey=${api}`)
        
-        //console.log(sources)
+      
 
         return sources.map((source) => source);
     } catch (error) {
@@ -25,15 +25,15 @@ export const fetchNews = async (request,page) => {
 
         if(request.category==='top-headlines'){
 
-            url = `https://corsanywhere.herokuapp.com/https://newsapi.org/v2/${request.category}?country=us&apiKey=${api}&pageSize=8&page=${page}`
+            url = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/${request.category}?country=us&apiKey=${api}&pageSize=8&page=${page}`
 
         }
       if (request.category==='keyword'){
-            url = `https://corsanywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${request.input}&pageSize=10&apiKey=${api}&page=${page}`
+            url = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${request.input}&pageSize=10&apiKey=${api}&page=${page}`
         }
 
         if(request.category==='everything'){
-            url = `https://corsanywhere.herokuapp.com/https://newsapi.org/v2/everything?sources=${request.source}&pageSize=10&apiKey=${api}&page=${page}`
+            url = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?sources=${request.source}&pageSize=10&apiKey=${api}&page=${page}`
           
 
         }
@@ -49,9 +49,6 @@ export const fetchNews = async (request,page) => {
     }
 };
 
-  // const { data: { articles } } = await axios.get(`${url}/${request.category}?${request.query}&pageSize=15&page=${request.page}&apiKey=${api}`);
-      //filter by sources:- https://newsapi.org/v2/everything?sources=reuters&apiKey=&pageSize=5&page=2
-      //search by keyword:-https://newsapi.org/v2/everything?q=ABC%20News&pageSize=10&apiKey=&page=1
-      //top headlines when component mounts :- https://newsapi.org/v2/top-headlines?country=us&apiKey=&pageSize=8&page=2
+ 
       // 1e54f63972d24682ac44eedaaf9c94a2
       //711fc8b76d3841deaed2593e353d4922
